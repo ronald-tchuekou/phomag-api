fdd360f0b1036fcfb14bbe2472b6cc51f0ae3c04
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2022-07-09 14:35:32 +0100

    new

--- api/routes/notification.route.js
+++ api/routes/notification.route.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const controller = require('../controllers/notification.controller')
-const authJWT = require("../middelwares/authJWT");
+const authJWT = require('../middelwares/authJWT')
 
 const router = express.Router()
 
-router.post('/notification',[authJWT.verifyToken], controller.createNotification)
-router.get('/notification/:user_id',[authJWT.verifyToken], controller.getNotifications)
-router.put('/notification/:id',[authJWT.verifyToken], controller.updateNotification)
-router.put('/notification/set-as/read',[authJWT.verifyToken], controller.markAsRead)
+router.post('/notification', [authJWT.verifyToken], controller.createNotification)
+router.get('/notification', [authJWT.verifyToken], controller.getNotifications)
+router.put('/notification/:id', [authJWT.verifyToken], controller.updateNotification)
+router.delete('/notification/:id', [authJWT.verifyToken], controller.deleteNotification)
+router.put('/notification/set-as/read', [authJWT.verifyToken], controller.markAsRead)
 
 module.exports = router
