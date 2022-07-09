f67737c74048c4e5b3652f13d27a643d74dde24a
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2022-07-09 13:15:07 +0100

    new

--- api/controllers/request.controller.js
+++ api/controllers/request.controller.js
@@ -1,6 +1,6 @@
 const RequestModel = require('../models/request.model')
 const PrinterModel = require('../models/printer-service.model')
-const UserModel  = require('../models/user.model')
+const UserModel = require('../models/user.model')
 const DisponibilityModel = require('../models/disponibility.model')
 const socket = require('../config/socket-client.config')
 const moment = require('moment')
@@ -86,16 +86,18 @@ exports.getPrinterRequestsById = async (req, res) => {
 exports.createRequest = async (req, res) => {
    try {
       const response = await RequestModel.createRequest(req.body)
-      const fetch = await UserModel.getUserWhere({user_id: req.body.author_id})
+      const fetch = await UserModel.getUserWhere({ user_id: req.body.author_id })
       const count = JSON.parse(req.body.document_list || '[]').length
       socket.emit('notify', {
          title: 'Request initialized',
-         message: `${fetch[0].lastname} ${fetch[0].firstname} are init a new request that contain ${count} document(s) to print.`,
+         message: `${fetch[0].sex === 'F' ? 'Md.' : 'Mr.'} ${fetch[0].lastname} ${
+            fetch[0].firstname
+         } are init a new request that contain ${count} document(s) to print.`,
          is_read: false,
          type: 'REQUEST',
          sender_id: `user_${fetch[0].user_id}`,
          receiver_id: `chief`,
-         request_id: response[0]
+         request_id: response[0],
       })
       res.json(response)
    } catch (e) {
@@ -107,9 +109,49 @@ exports.createRequest = async (req, res) => {
    }
 }
 
+const notifyThis = async (validator_id, status, request, printer_id) => {
+   let message = '',
+      title = ''
+   switch (status) {
+      case 'PRINTED':
+         const printer = await PrinterModel.getPrinterServiceWhere({ printer_service_id: printer_id })
+         title = 'Request printed'
+         message = `Printer service ${printer.service_name} are printed your request, the address of this service are : ${printer.service_address}.`
+         break
+
+      default:
+         const validator = (await UserModel.getUserWhere({ user_id: validator_id }))[0]
+         if (status === 'VALIDATE') {
+            message = `${validator.sex === 'F' ? 'Md.' : 'Mr.'} ${validator.lastname} ${
+               validator.firstname
+            } are validate your request that contain documents to print.`
+         } else {
+            title = 'Request canceled'
+            message = `${validator.sex === 'F' ? 'Md.' : 'Mr.'} ${validator.lastname} ${
+               validator.firstname
+            } are canceled your request that contain documents to print.`
+         }
+
+         break
+   }
+
+   socket.emit('notify', {
+      title: title,
+      message: message,
+      is_read: false,
+      type: 'REQUEST',
+      sender_id: validator_id ? 'user_' + validator_id : 'printer_' + printer_id,
+      receiver_id: request.author_id,
+      request_id: request.request_id,
+   })
+}
+
 exports.updateRequest = async (req, res) => {
    try {
-      const response = await RequestModel.updateRequest(req.body, req.params.id)
+      const res = await RequestModel.updateRequest(req.body, req.params.id)
+      const response = (await RequestModel.getRequestWhere({ request_id: res[0] }))[0]
+      if (req.body.validator_id || req.body.request_status === 'PRINTED')
+         notifyThis(req.body.validator_id, req.body.request_status, response, req.body.printer_id)
       res.json(response)
    } catch (e) {
       console.log(e)
@@ -181,6 +223,17 @@ exports.assignRequest = async (req, res) => {
          },
          request_id
       )
+
+      socket.emit('notify', {
+         title: 'New request assigned',
+         message:
+            'The new request is validate and assigned to you. Please try to printed it and set to this app that you are finish to print it.',
+         is_read: false,
+         type: 'REQUEST',
+         sender_id: 'user_' + req.user_id,
+         receiver_id: 'printer_' + valid_availabilities[0].printer_id,
+         request_id: request_id,
+      })
 
       res.json(response)
    } catch (e) {
