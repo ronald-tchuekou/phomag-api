96294b50593ac1424d9ee67b8fb00c430338420e
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2022-07-09 11:10:50 +0100

    new

--- api/models/request.model.js
+++ api/models/request.model.js
@@ -54,7 +54,7 @@ exports.getRequestWhere = async (query) =>
       .from(tableName)
       .where(query)
       .select()
-      .orderBy('created_at', 'desc')
+      .orderBy(tableName + '.created_at', 'desc')
 
 /**
  * @param document
@@ -77,7 +77,7 @@ exports.getAllRequest = async () =>
    await DBInstance.join(UserTableName, tableName + '.author_id', UserTableName + '.user_id')
       .from(tableName)
       .select()
-      .orderBy('created_at', 'desc')
+      .orderBy(tableName + '.created_at', 'desc')
 
 /**
  * @param request_id
